842e4e4586b081da21a059ac2587346e7669b20f
tandemic97 <tandemic97@gmail.com>
2022-05-12 17:57:11 +0300

    change writebyte to writerune

--- reverse_string.go
+++ reverse_string.go
@@ -4,8 +4,8 @@ import "strings"
 
 func ReverseString(input string) string {
 	var sb strings.Builder
-	for i := len(input) - 1; i >= 0; i-- {
-		sb.WriteByte(input[i])
+	for i := len([]rune(input)) - 1; i >= 0; i-- {
+		sb.WriteRune([]rune(input)[i])
 	}
 	return sb.String()
 }
