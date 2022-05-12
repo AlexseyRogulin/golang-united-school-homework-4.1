f9b94495b9d62c7fbfcdfbc574e55eee11c67c59
tandemic97 <tandemic97@gmail.com>
2022-05-12 17:52:01 +0300

    first commit

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
-func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+import "strings"
+
+func ReverseString(input string) string {
+	var sb strings.Builder
+	for i := len(input) - 1; i >= 0; i-- {
+		sb.WriteByte(input[i])
+	}
+	return sb.String()
 }
